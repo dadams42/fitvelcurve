5739d308a86161b0ec8832077275ee8d1fde49c0
Sasha Levina <sashalvna@gmail.com>
2024-07-18 16:35:30 -0500

    revert gal_class, add plotting, remove print statement in tests

--- fitvelcurve/gal_class.py
+++ fitvelcurve/gal_class.py
@@ -4,45 +4,7 @@ from astropy.constants import G, M_sun, kpc
 from astropy import units as u
 from astropy.cosmology import Planck15 as cosmo
 import dynesty
-
 from dynesty import plotting as dyplot
-
-### alternative functions for mass_nfw / constants etc.
-import numpy as np
-from numpy import pi, log
-
-
-#some constants
-h = 0.678 #Gaia 2016
-
-G = 6.67430e-11
-
-Msun = 1.98847e30
-kpc = 3.0856775814913673e19
-
-fac = 102
-
-rhocrit = 3*(100*h/(kpc))**2/(8*np.pi*G) * kpc**3 / Msun
-
-def delc(c):
-    return (fac/3)*c**3/(log(1+c)-c/(1+c))
-    
-
-def mass_nfw(r, mvir, cvir):
-    
-    rv = (mvir/((4/3) * pi * fac * rhocrit))**(1/3)
-    
-    rs = rv / cvir
-    
-    rhos = rhocrit*delc(cvir)
-    
-    return 4 * pi *  rhos * rs**3 * (log((r + rs) / rs) + rs/(r + rs) - 1) 
-
-def velocity(x, mass, c):
-    return np.sqrt(G * Msun * mass_nfw(x, mass, c) / x/kpc) / 1e3    
-
-###
-
 
 class Galaxy():
     """
@@ -136,23 +98,17 @@ class Galaxy():
         log_m_h, c = x
 
         # Redefine the class attributes to match these
-        #self.mvir = 10**log_m_h
+        self.mvir = 10**log_m_h
         #self.cvir = c
         
         # Calculate enclosed mass array with NFW profile
-        #enclosed_mass = self.mass_nfw()
-
-  
+        enclosed_mass = self.mass_nfw()
 
         # Calculate predicted velocity from v^2 = GM/r
-        #pred_vel = np.sqrt(G.value * enclosed_mass / self.data_err)
-
-        pred_vel = velocity(self.data_r, 10.0**x[0], x[1])
+        pred_vel = np.sqrt(G.value * enclosed_mass / self.data_err)
 
         # Calculate log-likelihood
-        #llh = -1/2 * np.sum(((self.data_vel - pred_vel) / self.data_err) ** 2)
-
-        llh = -0.5 * np.sum((self.data_vel - pred_vel)**2 / self.data_err**2)
+        llh = -1/2 * np.sum(((self.data_vel - pred_vel) / self.data_err) ** 2)
 
         return llh
 
@@ -194,18 +150,13 @@ class Galaxy():
 
         print("number of iterations = ", resu.niter)
 
-
-# Plot a summary of the run.
+        # Plot a summary of the run.
         rfig, raxes = dyplot.runplot(resu)
 
-# Plot traces and 1-D marginalized posteriors.
+        # Plot traces and 1-D marginalized posteriors.
         tfig, taxes = dyplot.traceplot(resu)
 
-# Plot the 2-D marginalized posteriors.
+        # Plot the 2-D marginalized posteriors.
         cfig, caxes = dyplot.cornerplot(resu)
 
         return sampler
-    
-
-
-    
