29e623b9909219cd0a0da63552b1106cb2156f0d
David Robinson <drobins4@swarthmore.edu>
2024-07-18 11:33:27 -0500

    Add tests directory and framework for two unit tests

---
+++ tests/test_gal_class.py
@@ -0,0 +1,13 @@
+import pytest
+import numpy as np
+import astropy
+
+def test_ptform()
+    '''
+    Check the output of ptform() on a few specific cases
+    '''
+
+def test_rho_nfw()
+    '''
+    Check the NFW density profile in the Galaxy() class vs. the built-in astropy version
+    '''
